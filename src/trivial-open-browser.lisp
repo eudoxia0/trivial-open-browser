7ab4743dea9d592639f15c565bfa0756e828c427
Fernando Borretti <eudoxiahp@gmail.com>
2016-07-01 20:19:12 -0300

    [Fix #2] Add a feature test for CCL

--- src/trivial-open-browser.lisp
+++ src/trivial-open-browser.lisp
@@ -7,11 +7,11 @@
 (in-package :trivial-open-browser)
 
 (defparameter +format-string+
-  #+(or win32 mswindows)
+  #+(or win32 mswindows windows)
   "explorer ~S"
   #+(or macos darwin)
   "open ~S"
-  #-(or win32 mswindows macos darwin)
+  #-(or win32 mswindows macos darwin windows)
   "xdg-open ~S")
 
 (defun open-browser-through-shell (url)
